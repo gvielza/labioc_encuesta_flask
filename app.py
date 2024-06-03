7ce06ce163ef85109423f4c1818fd322959254a8
geo <voltagnelly@gmail.com>
2024-06-02 21:49:38 -0300

    sqlite3

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from base_datos.conexion import Conexion
 
 app=Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return "Hello World eestaré hacidno todo aca"
+    return render_template('index.html')
+
+@app.route('/resultados', methods=['POST'])
+def saludo():
+    dni = request.form['dni']
+    usuario=request.form['usuario']
+    contrasenna=request.form['contrasenna']
+    conexion=Conexion("base_datos/usuarios.db")
+    conexion.crear_tabla_cliente()
+    conexion.agregar_cliente(dni,usuario,contrasenna)
+    clientes=conexion.mostrar_clientes()
+    conexion.cerrar_conexion()
+    return render_template('resultados.html', dni=dni, usuario=usuario,clientes=clientes)
