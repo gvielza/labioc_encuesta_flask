3fa9a9357720133fc8fc63f4c8d985b0077c0e06
geo <voltagnelly@gmail.com>
2024-06-03 23:00:44 -0300

    back completo

--- app.py
+++ app.py
@@ -11,9 +11,72 @@ def index():
     return render_template('index.html')
 
 
+def litros_agua_por_anno(canilla, ducha, manos, dientes, platos, lavarropas, descarga, perdidas, coche,vereda, ropa, vaca,
+                         pollo, cerdo,frutas,verduras):
+    cantidad=0
+    if(canilla=="foto1"):
+        cantidad=int(ducha)*5475+int(manos)*16425 #27375
+    else:
+        cantidad=int(ducha)*2737+int(manos)*8212
+    if(dientes=="si" and canilla=="foto1"):
+        cantidad=cantidad+10950
+    if(dientes=="no" and canilla=="foto1"):
+        cantidad=cantidad+32850
+    if(dientes=="si" and canilla=="foto2"):
+        cantidad=cantidad+5475
+    if(dientes=="no" and canilla=="foto2"):
+        cantidad=cantidad+16425
+    if(platos=="1" and canilla=="foto1"):
+        cantidad=cantidad+54750
+    if(platos=="1" and canilla=="foto2"):
+        cantidad=cantidad+27375
+    if(platos=="2" and canilla=="foto1"):
+        cantidad=cantidad+164250
+    if(platos=="2" and canilla=="foto2"):
+        cantidad=cantidad+82125
+    if(platos=="3"):
+        cantidad=cantidad+5475
+    cantidad=cantidad+int(lavarropas)*2346
+    if(descarga=="si"):
+        cantidad=cantidad+4380
+    else:
+        cantidad=cantidad+7665
+    if(perdidas=="si"):
+        cantidad=cantidad+1577
+    cantidad=cantidad+int(coche)*1200
+    if(vereda=="1"):
+        cantidad=cantidad+11700
+    if(vereda=="2"):
+        cantidad=cantidad+3900
+    if(vereda=="3"):
+        cantidad=cantidad+100
+    cantidad=cantidad+int(ropa)*2700
+    cantidad=cantidad+int(vaca)*806000
+    cantidad=cantidad+int(pollo)*182000
+    cantidad=cantidad+int(cerdo)*249600+int(frutas)*31200+int(verduras)*15600
+
+
+    return cantidad
+
+
+def mensaje_por_consumo(litros):
+    consumo_mundial=1240000
+    litros_x_acuario=500
+    acuarios=int(litros)/litros_x_acuario
+
+    if(consumo_mundial-litros<50000 and consumo_mundial-litros>0):
+        return "Buen trabajo!Tu consumo de agua es similar al promedio mundial. Pequeños cambios pueden hacer una gran diferencia."
+    if(consumo_mundial<litros):
+        return f"¡Wow! Con tu consumo podrías llenar  {acuarios} acuarios!"
+    if(consumo_mundial>litros):
+        return  "Impresionante!Tu consumo es menor que el promedio mundial. Sigue así para ayudar a conservar nuestros recursos."
+
+
+
 @app.route('/resultados', methods=['POST'])
 def resultados():
     nombre = request.form['nombre']
+    edad= request.form['edad']
     ciudad = request.form['ciudad']
     canilla = request.form['canilla']
     ducha = request.form['ducha']
@@ -29,20 +92,25 @@ def resultados():
     pollo= request.form['pollo']
     cerdo= request.form['cerdo']
     vereda= request.form['vereda']
-
-
-
-
+    frutas= request.form['frutas']
+    verduras= request.form['verduras']
+    litros = litros_agua_por_anno(canilla, ducha, manos, dientes, platos, lavarropas, descarga, perdidas, coche, vereda,
+                                  ropa, vaca, pollo, cerdo, frutas, verduras)
     conexion = Conexion("base_datos/usuarios.db")
     conexion.crear_tabla_usuarios()
-    conexion.agregar_usuario(nombre, ciudad)
-    usuarios=conexion.mostrar_usuarios()
+    conexion.agregar_usuario(edad, ciudad, canilla, ducha, manos, dientes, platos, lavarropas, descarga, perdidas, coche, ropa, vaca, pollo, cerdo, vereda, frutas, verduras,litros)
+    #usuarios=conexion.mostrar_usuarios()
 
+    litros_f=f"{litros:,}".replace(",", ".")
+    mensaje=mensaje_por_consumo(litros)
+    if(litros>1240000):
+        imagen="triste.avif"
+    else:
+        imagen="feliz.jpg"
+
+    print(litros_f)
     conexion.cerrar_conexion()
-    return render_template('resultados.html', nombre=nombre,ciudad=ciudad,canilla=canilla,
-                           ducha=ducha,vereda=vereda
-                           ,manos=manos,dientes=dientes,platos=platos,lavarropas=lavarropas,descarga=descarga,
-                           perdidas=perdidas,coche=coche,ropa=ropa,vaca=vaca,pollo=pollo,cerdo=cerdo,usuarios=usuarios)
+    return render_template('resultados.html', nombre=nombre, litros_f=litros_f, mensaje=mensaje,imagen=imagen)
 
 
 
--- base_datos/conexion.py
+++ base_datos/conexion.py
@@ -9,11 +9,11 @@ class Conexion:
 
     def crear_tabla_usuarios(self):
         self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS usuarios( nombre TEXT,ciudad TEXT)")
+            "CREATE TABLE IF NOT EXISTS usuarios( edad TEXT,ciudad TEXT, canilla TEXT,ducha TEXT,manos TEXT,dientes TEXT,platos TEXT,lavarropas TEXT,descarga TEXT,perdidas TEXT,coche TEXT,ropa TEXT,vaca TEXT,pollo TEXT,cerdo TEXT,vereda TEXT,frutas TEXT,verduras TEXT,litros TEXT)")
         self.conexion.commit()
 
-    def agregar_usuario(self, nombre, ciudad):
-        self.cursor.execute("INSERT INTO usuarios VALUES(?,?)", (nombre,ciudad))
+    def agregar_usuario(self, edad,ciudad,canilla,ducha,manos,dientes,platos,lavarropas,descarga,perdidas,coche,ropa,vaca,pollo,cerdo,vereda,frutas,verduras,litros):
+        self.cursor.execute("INSERT INTO usuarios VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (edad,ciudad,canilla,ducha,manos,dientes,platos,lavarropas,descarga,perdidas,coche,ropa,vaca,pollo,cerdo,vereda,frutas,verduras,litros))
 
         self.conexion.commit()
     def mostrar_usuarios(self):
